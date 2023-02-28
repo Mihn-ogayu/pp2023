5e347a1485d1daf59fcfcc0c625ddff52c894c08
Mihn-Ogayu <trinhtienminh.pro@gmail.com>
2023-02-28 19:23:53 +0700

    Updated the code

--- 1.student.mark.py
+++ 1.student.mark.py
@@ -1,22 +1,64 @@
+#function for input student
+def input_students():
+    no_st = int(input("Number of students: "))
+    if no_st <= 0:
+        print("Invalid number")
+        exit()
+    else:
+        stu_list = [[0 for i in range(3)] for j in range(no_st)]
+        for i in range(no_st):
+            stu_list[i][0] = input("Student ID: ")
+            stu_list[i][1] = input("Student Name: ")
+            stu_list[i][2] = input("DOB:")
+    return stu_list
 
-students ={ }
-courses = { }
-marks = { }
-def input_student():
-    n = int(input("Enter number of students: "))
-    for i in range (n):
-        student_id = input("Student ID: "),
-        student_name = input("Student name: "),
-        student_dob = input("DOB: ")]
-        
-def input_course():
-    m = int(input("Enter number of courses: "))
-    for j in range(0,m):
-        course_info = [input("Course ID: "),input("Course name: ")]
-        
-input_student(students)
-input_course(courses)
-print(students)
-print(courses)
-# input_student(n)
-# input_course(m)
+#function for input corse
+def input_courses():
+    no_co = int(input("Number of courses: "))
+    if no_co <= 0:
+        print("Invalid course")
+        exit()
+    else:
+        co_list = [[0 for i in range (2)] for j in range(no_co)]
+        for i in range(no_co):
+            co_list[i][0] = input("Course ID: ")
+            co_list[i][1] = input("Course name: ")
+    return co_list
+
+#List the students
+def list_students():
+    for i in range(len(student_list)):
+        print("Student ID: ",student_list[i][0])
+        print("Name: ",student_list[i][1])
+        print("DOB: ",student_list[i][2])
+
+#List the courses
+def list_courses():
+    for i in range(len(course_list)):
+        print("Course ID: ",course_list[i][0])
+        print("Name: ",course_list[i][1])
+
+#input  student marks for subjects
+def input_marks(co,stu):
+    mark = [[0 for i in range(len(stu))] for j in range(len(co))]
+    for i in range(len(co)):
+        for j in range(len(stu)):
+            mark [i][j] = int(input("Mark for student",stu[i][1],"for subject",co[i][1]))
+    return mark
+
+#Show student marks function
+def show_student_marks(stu,co):
+    courseID = input("Course ID")
+    for i in range(len(co)):
+        if courseID == co[i][1]:
+            print("Student ",stu[i][1],"Course:",co[i][1],"Mark: ",mark[i][j])  
+        else:
+            exit()
+
+#define student and course lists
+student_list = input_students()
+course_list = input_courses()
+
+list_students()
+list_courses()
+
